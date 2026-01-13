330da6da04e6fc0ab0b283bdc2946bf4c745aa6c
LionTurtle-dev <patakitasbalint@gmail.com>
2026-01-13 18:40:24 +0100

    This is the main program. At now just in hungarian language, but english one is coming soon.
    This is the main program. At now just in hungarian language, but english one is coming soon.(Wrong file has been removed.)

---
+++ json_indent.py
@@ -0,0 +1,24 @@
+##Json file indentater python script
+import json
+
+
+
+def indent(fl: str) -> None:
+    v = None
+    with open(fl, encoding='utf-8') as file:
+        v = json.load(file)
+
+
+    with open(fl, 'w', encoding='utf-8') as f:
+        json.dump(v, f, indent=4)
+
+while (fl := input('Add meg a Json file útvonalát! -> ')) != "":
+    try:
+        indent(fl)
+    except json.decoder.JSONDecodeError:
+        print(f'A(z) {fl} nem Json file!')
+    except FileNotFoundError:
+        print(f'A(z) „{fl}”, nevű file nem található!')
+    else:
+        print("Behúzás sikeres.")
+        break
